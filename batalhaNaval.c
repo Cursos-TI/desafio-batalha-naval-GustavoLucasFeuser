1c0d28cdce729a5d059aa9ca7159c933a65958b9
Gustavo Lucas Feuser <125416702+GustavoLucasFeuser@users.noreply.github.com>
2025-02-08 22:52:51 +0000

    desafio feito

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,58 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+#define TAMANHO_TABULEIRO 10
+#define TAMANHO_NAVIO 3
+
+void exibirTabuleiro(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]) {
+    printf("\nTabuleiro:\n");
+    for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
+        for (int j = 0; j < TAMANHO_TABULEIRO; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void aplicarHabilidade(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO], int x, int y, char tipo) {
+    if (tipo == 'C') { // Cone
+        if (x - 2 >= 0) tabuleiro[x - 2][y] = 1;
+        if (x - 1 >= 0) {
+            if (y - 1 >= 0) tabuleiro[x - 1][y - 1] = 1;
+            tabuleiro[x - 1][y] = 1;
+            if (y + 1 < TAMANHO_TABULEIRO) tabuleiro[x - 1][y + 1] = 1;
+        }
+        if (y - 2 >= 0) tabuleiro[x][y - 2] = 1;
+        if (y - 1 >= 0) tabuleiro[x][y - 1] = 1;
+        tabuleiro[x][y] = 1;
+        if (y + 1 < TAMANHO_TABULEIRO) tabuleiro[x][y + 1] = 1;
+        if (y + 2 < TAMANHO_TABULEIRO) tabuleiro[x][y + 2] = 1;
+    } else if (tipo == 'O') { // Octaedro
+        if (x - 1 >= 0) tabuleiro[x - 1][y] = 1;
+        if (y - 1 >= 0) tabuleiro[x][y - 1] = 1;
+        tabuleiro[x][y] = 1;
+        if (y + 1 < TAMANHO_TABULEIRO) tabuleiro[x][y + 1] = 1;
+        if (x + 1 < TAMANHO_TABULEIRO) tabuleiro[x + 1][y] = 1;
+    } else if (tipo == 'X') { // Cruz
+        if (x - 1 >= 0) tabuleiro[x - 1][y] = 1;
+        if (y - 2 >= 0) tabuleiro[x][y - 2] = 1;
+        if (y - 1 >= 0) tabuleiro[x][y - 1] = 1;
+        tabuleiro[x][y] = 1;
+        if (y + 1 < TAMANHO_TABULEIRO) tabuleiro[x][y + 1] = 1;
+        if (y + 2 < TAMANHO_TABULEIRO) tabuleiro[x][y + 2] = 1;
+        if (x + 1 < TAMANHO_TABULEIRO) tabuleiro[x + 1][y] = 1;
+    }
+}
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO] = {0};
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    // Aplicação das habilidades no tabuleiro
+    aplicarHabilidade(tabuleiro, 4, 4, 'C'); // Cone
+    aplicarHabilidade(tabuleiro, 7, 7, 'O'); // Octaedro
+    aplicarHabilidade(tabuleiro, 2, 2, 'X'); // Cruz
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
+    // Exibir tabuleiro com habilidades aplicadas
+    exibirTabuleiro(tabuleiro);
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
-
     return 0;
 }
